2945f20fc3cc5e65ac77d642e6b2f1381c0330f2
acwu11 <73973108+acwu11@users.noreply.github.com>
2020-11-15 11:56:01 -0800

    kitti bin to pcd tests

---
+++ kitti_conversion.py
@@ -0,0 +1,30 @@
+import numpy as np
+import struct 
+import open3d as o3d
+import os
+
+def convert_kitti_bin_to_pcd(binFilePath, outputPath):
+    size_float = 4
+
+    for i in range(len(os.listdir(binFilePath))):
+        list_pcd = []
+        bf = binFilePath+"{i}.bin".format(i=str(i).zfill(10))
+        nf = outputPath+f"{i}.pcd"
+    
+        try:
+            with open(bf, "rb") as f: 
+                byte = f.read(size_float*4)
+                while byte:
+                    x, y ,z, intensity = struct.unpack("ffff", byte)
+                    list_pcd.append([x, y, z])
+                    byte = f.read(size_float * 4)
+            np_pcd = np.asarray(list_pcd)
+            pcd = o3d.geometry.PointCloud()
+            v3d = o3d.utility.Vector3dVector
+            pcd.points = v3d(np_pcd)
+            o3d.io.write_point_cloud(nf, pcd)
+        except:
+            pass
+
+if __name__ == "__main__":
+    convert_kitti_bin_to_pcd('velodyne_points/data/', 'velodyne_points/data_pcd/')
